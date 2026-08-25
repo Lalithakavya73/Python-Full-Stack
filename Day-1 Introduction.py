0534a57c646e40e742abf7daa3d2bb1c9b391e5f
Lalithakavya73 <lalithakavyan.2703@gmail.com>
2026-08-25 20:47:27 +0530

    Day 1 of my Python Full Stack Development journey at Codegnan
    I started by learning the Introduction to Python, including:
    * What is Python?
    * Features and advantages of Python
    * Applications of Python
    * Why Python is widely used in software development
    This is just the beginning, and I’m excited to keep learning, practicing, and improving my development skills step by step.

---
+++ Day-1 Introduction.py
@@ -0,0 +1,85 @@
+'''
+what is program?
+--> program is the process of instructions given by the user or programmer to planning what to do and how to do
+
+Example:
+'''
+user_en = 2000
+bal_ = 10000
+result = bal_ -user_en
+print(result)
+'''
+There are 2 types of languages:
+
+1. Procedural Language : "Bild on the functions"
+--> An procedural language is organized mainly on functions
+Example:
+c
+'''
+def cal_ad(a,b):
+    return a + b
+a = 7
+b = 9
+any_ = cal_ad(a,b)
+print(any_)
+'''
+
+2. Object-Oriented programming (object oriented programming system(oops)) :
+--> Object-oriented programming is organized mainly on classes and object
+Example:
+python
+'''
+num = 7
+print(type(num))
+'''
+
+What is python?
+--> python is High level language
+python take care about memory allocation 
+Example:
+'''
+num = 9
+print(type(num))
+print(id(num))
+'''
+Interpreter language
+--> Line by line execution is followed by python that is the reason we call it as interpreter language
+Example:
+'''
+num = 9
+num_2 = 90
+print(num)
+print(type(num))
+print(id(num))
+'''
+Dynamically typed language
+--> No need to mention the type of data passing to the variable
+Example:
+'''
+num = 78
+any_ = ['Python']
+nums = [1,2]
+all_ = (1,2)
+print(type(num))
+print(type(any_))
+print(type(nums))
+print(type(all_))
+'''
+Why python?
+--> 1. More Libraries
+    2. It's aCross-Platform 
+    3. Open-Source
+    4. Simple syntax than other programming languages
+
+Applications:
+Use in Data Science
+Web Development
+AI
+Machine Learning
+Deep Learning
+
+History of Python
+>It was started in early 1980s, and released in the years 1991 by Guido Van Rossum 
+>And he pick the from the most loved series called monty python 
+>The first version was released in the year 1991 which is python 0.9.0 and now the version we are using 3.14
+'''
