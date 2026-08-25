785c4a4392eae14f4798005d31372dbdeb8510b5
Lalithakavya73 <lalithakavyan.2703@gmail.com>
2026-08-25 20:50:36 +0530

    Today I learned some important Python fundamentals and practiced writing basic Python programs.
    Topics Covered:
    🔹 Identifiers
    🔹 Keywords
    🔹 Literals
    🔹 Operators
    🔹 Statements
    🔹 Single-line & Multi-line Comments
    🔹 Variable Naming Rules
    🔹 Dictionaries
    🔹 Multiple Variable Assignment
    🔹 Swapping Two Variables
    💡 I’m focusing on building a strong foundation in Python step by step, as part of my journey toward becoming a Python Full Stack Developer.

---
+++ Day-2.py
@@ -0,0 +1,119 @@
+
+'''
+Tokens
+-->Tokens are the small unit in the python...
+
+1.Identifier
+------------
+--> Identifier is a od variable or function or class...
+
+Variable
+'''
+num = 'Pythoon'
+print (type(num))
+'''
+Functions
+'''
+def add_(a,b):
+    print(a+b)
+add_(4-5)
+'''
+Class
+class details:
+      pass
+per_1 = details
+
+2.Keywords
+----------
+--> keywords are already saved in python for an specified reason to run...
+Example:
+if
+else
+for
+while
+return
+print
+
+3. Literals
+-----------
+--> Literals are the datatypes that need to be stored in variables...
+'''
+num = 90
+name = 'kavya'
+'''
+4.OPerator
+----------
++, -, =,
+
+Statements
+----------
+--> Statements are the instructions given to the program...
+'''
+num = 90
+age = 2
+if age >= 18:
+   print(age)
+'''
+Comments
+--------
+--> Oncee comments are open the lines inside will never execute in python file
+1.Single line comments(#)
+------------------------
+--> Used to comment only one line
+Example:
+age = 20
+if age >= 18: #this check age is greater or equal
+   print(age)
+
+2.Multi-line(''' ''', """ """)
+------------------------------
+--> Used to comment more than one line
+
+Variables Rules
+---------------
+--> can't use number at first position
+--> Can't use speical character anywhere
+--> can't use space
+--> keywords
+Example:
+2num = 90
+$num = 89
+n um = 78
+if = 67
+
+Good ways
+---------
+--> Small letters and captail letters, (_) under
+Example:
+nUm_1 = 90
+Num_2 = 78
+kavya_chakradhar = 90
+Example:
+a = {'name' : 'kavya',
+      'AC_num' : '44567890987654'}
+b = {'name' : 'chakradhar',
+      'AC_num' : '44567890987690}
+c = {'name' : 'lalitha',
+      'AC_num' : '44567890987678}
+
+SBI_kavya_details = {'name':'kavya',
+    'AC_num' : '44567890987654'}
+
+ICIC_chakradhar_details = {'name':'chakradhar',
+      'AC_num' : '44567890987690}
+
+num = 90
+print(num)
+
+num_3, num_2 = 56, 89
+print(num_3)
+print(num_2)
+
+Swapping of two variables
+a,b = 45, 67
+print('a' =, a)
+print('b' = ,b)
+a, b = b, a
+print('a' =, a)
+print('b' = ,b)
+'''
